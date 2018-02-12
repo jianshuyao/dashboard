d41f361fcdb00b94f85e8faee50be5b38c6e53f1
jianshuyao <36392426+jianshuyao@users.noreply.github.com>
2018-02-12 11:35:58 +0800

    Updated the Firebase project config.

--- src/containers/Full/Full.js
+++ src/containers/Full/Full.js
@@ -29,12 +29,12 @@ import * as firebase from 'firebase';
 // TODO: Replace with your project's customized code snippet
 
 var config = {
-    apiKey: "AIzaSyBPweAHcP4em0PYh8XWzEqjNhTk3OBNS5I",
-    authDomain: "dashboard-dev-8b843.firebaseapp.com",
-    databaseURL: "https://dashboard-dev-8b843.firebaseio.com",
-    projectId: "dashboard-dev-8b843",
-    storageBucket: "dashboard-dev-8b843.appspot.com",
-    messagingSenderId: "264557605189"
+    apiKey: "AIzaSyDGVvjYWZ7bvragljtng1sEDJsxKPKz1hU",
+    authDomain: "week-767b2.firebaseapp.com",
+    databaseURL: "https://week-767b2.firebaseio.com",
+    projectId: "week-767b2",
+    storageBucket: "",
+    messagingSenderId: "1082072428898"
   };
 firebase.initializeApp(config);
 
